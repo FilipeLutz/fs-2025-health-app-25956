ba3b627c499df3c5bd2075d9c9cc1dd9bc80d1ea
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-04 09:03:11 +0100

    Create PatientDoctor.cs

---
+++ HealthApp.Domain/PatientDoctor.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthApp.Domain
+{
+    public class Patient
+    {
+        public string Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Email { get; set; }
+        public object Name { get; internal set; }
+        public string UserId { get; internal set; }
+    }
+}
