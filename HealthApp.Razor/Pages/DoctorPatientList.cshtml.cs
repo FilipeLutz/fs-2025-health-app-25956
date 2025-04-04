9082dfd00d53f5a6f2959108dc1ac707dbbc9429
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-04 09:07:22 +0100

    Create DoctorPatientList.cshtml.cs

---
+++ HealthApp.Razor/Pages/DoctorPatientList.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HealthApp.Razor.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HealthApp.Razor.Pages
+{
+    [Authorize(Roles = "Admin")]
+    public class DoctorPatientListModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DoctorPatientListModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<DoctorPatient> DoctorPatients { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            DoctorPatients = await _context.DoctorPatients
+                .Include(d => d.Doctor)
+                .Include(d => d.Patient).ToListAsync();
+        }
+    }
+}
