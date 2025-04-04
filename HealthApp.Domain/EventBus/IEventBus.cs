3e30c8f74d3099950e6563c81e4ad31e43cae9e5
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-04 09:02:49 +0100

    Create IEventBus.cs

---
+++ HealthApp.Domain/EventBus/IEventBus.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthApp.Domain.EventBus;
+
+public interface IEventBus
+{
+    void Publish(string queueName, string message);
+    void Subscribe(string queueName, Action<string> handler);
+    void Dispose();
+}
