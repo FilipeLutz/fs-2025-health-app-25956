ae907df09927fc9200c848852a576780fc8c2aeb
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-06 20:16:09 +0100

    Create EditUser.cs

---
+++ HealthApp.Domain/Entities/EditUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthApp.Domain.Entities
+{
+    public class EditUserViewModel
+    {
+        public string UserId { get; set; } 
+        public string Name { get; set; } 
+        public string Email { get; set; } 
+        public List<RoleSelectionModel> Roles { get; set; } 
+    }
+
+    public class RoleSelectionModel
+    {
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; } 
+    }
+}
