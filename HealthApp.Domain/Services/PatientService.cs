f7bcee2b5918a8aad2ac40d374eb481c8f402211
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:29:43 +0100

    Create PatientService.cs

---
+++ HealthApp.Domain/Services/PatientService.cs
@@ -0,0 +1,45 @@
+﻿using HealthApp.Domain.Entities;
+using HealthApp.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthApp.Domain.Services;
+
+public class PatientService : IPatientService
+{
+    private readonly ApplicationDbContext _context;
+
+    public PatientService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Patient> GetPatientByIdAsync(int id)
+    {
+        return await _context.Patients
+            .Include(p => p.User)
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<IEnumerable<Patient>> GetAllPatientsAsync()
+    {
+        return await _context.Patients
+            .Include(p => p.User)
+            .ToListAsync();
+    }
+
+    public async Task UpdatePatientAsync(Patient patient)
+    {
+        _context.Patients.Update(patient);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Appointment>> GetPatientAppointmentsAsync(int patientId)
+    {
+        return await _context.Appointments
+            .Where(a => a.PatientId == patientId)
+            .Include(a => a.Doctor)
+            .ThenInclude(d => d.User)
+            .OrderByDescending(a => a.AppointmentDateTime)
+            .ToListAsync();
+    }
+}
