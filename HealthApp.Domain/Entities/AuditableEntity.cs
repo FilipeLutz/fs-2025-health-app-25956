7baa11bce852f323296832e7a86c15c2b1891aee
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:28:28 +0100

    Create AuditableEntity.cs

---
+++ HealthApp.Domain/Entities/AuditableEntity.cs
@@ -0,0 +1,7 @@
+﻿public abstract class AuditableEntity
+{
+    public DateTime CreatedAt { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+    public string ModifiedBy { get; set; }
+}
