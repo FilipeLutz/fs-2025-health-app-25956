bee1ae03ee88edb408a5573620a715747b36439b
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:28:21 +0100

    Create ApplicationUser.cs

---
+++ HealthApp.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthApp.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public Patient Patient { get; set; }
+        public Doctor Doctor { get; set; }
+    }
+}
