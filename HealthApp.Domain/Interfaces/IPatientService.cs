2df4814d767dffbf71bb7ca201e4dd5cecb60c9e
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:29:05 +0100

    Create IPatientService.cs

---
+++ HealthApp.Domain/Interfaces/IPatientService.cs
@@ -0,0 +1,11 @@
+﻿using HealthApp.Domain.Entities;
+
+namespace HealthApp.Domain.Interfaces;
+
+public interface IPatientService
+{
+    Task<Patient> GetPatientByIdAsync(int id);
+    Task<IEnumerable<Patient>> GetAllPatientsAsync();
+    Task UpdatePatientAsync(Patient patient);
+    Task<IEnumerable<Appointment>> GetPatientAppointmentsAsync(int patientId);
+}
