beb4ac326e3c738688d3f890a5afba853d01d953
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:29:08 +0100

    Create IPrescriptionService.cs

---
+++ HealthApp.Domain/Interfaces/IPrescriptionService.cs
@@ -0,0 +1,16 @@
+﻿using HealthApp.Domain.Entities;
+namespace HealthApp.Domain.Interfaces
+{
+    public interface IPrescriptionService
+    {
+        Task<Prescription> GetByIdAsync(int id);
+        Task<IEnumerable<Prescription>> GetByPatientIdAsync(int patientId);
+        Task<IEnumerable<Prescription>> GetByDoctorIdAsync(int doctorId);
+        Task CreateAsync(Prescription prescription);
+        Task UpdateAsync(Prescription prescription);
+        Task DeleteAsync(int id);
+        Task<bool> CheckForInteractions(string medication, int patientId);
+        Task<IEnumerable<Prescription>> GetAllAsync();
+        Task<bool> RenewAsync(int id);
+    }
+}
