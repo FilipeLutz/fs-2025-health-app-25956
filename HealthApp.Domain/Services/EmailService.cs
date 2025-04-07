45d5e4e513ec4264d5b7d1d306f7c6dc73641517
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:29:38 +0100

    Create EmailService.cs

---
+++ HealthApp.Domain/Services/EmailService.cs
@@ -0,0 +1,20 @@
+﻿using HealthApp.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace HealthApp.Domain.Services;
+
+public class EmailService : IEmailService
+{
+    private readonly ILogger<EmailService> _logger;
+
+    public EmailService(ILogger<EmailService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task SendEmailAsync(string email, string subject, string message)
+    {
+        _logger.LogInformation($"Email to {email}: {subject} - {message}");
+        return Task.CompletedTask;
+    }
+}
