493969ae0da1851f89a0d1fb9337babeee619875
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:28:14 +0100

    Create ReportsController.cs

---
+++ HealthApp.API/Controllers/ReportsController.cs
@@ -0,0 +1,25 @@
+﻿using HealthApp.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace HealthApp.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IAppointmentRepository _appointmentService;
+        private readonly IDoctorService _doctorService;
+
+        public ReportsController(
+            IAppointmentRepository appointmentService,
+            IDoctorService doctorService)
+        {
+            _appointmentService = appointmentService;
+            _doctorService = doctorService;
+        }
+    }
+}
