9aabf5f71141b55e3fd3ce30569a8c19b9ba6297
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 00:28:25 +0100

    Create AppointmentCreateDto.cs

---
+++ HealthApp.Domain/Entities/AppointmentCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthApp.Domain.Entities;
+
+public class AppointmentCreateDto
+{
+    public int DoctorId { get; set; }
+    public int PatientId { get; set; }
+    public DateTime AppointmentDateTime { get; set; }
+    public string Reason { get; set; }
+}
