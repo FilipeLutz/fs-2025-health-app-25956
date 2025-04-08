2864f7354f7bea1fabfb8f5685e4f568169a6ec4
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 12:55:48 +0100

    Revert "Update IAppointmentRepository.cs"
    This reverts commit ace3542e8ddeaa069df395eef3d9c8d6c4dc4cd2.

--- HealthApp.Domain/Interfaces/IAppointmentRepository.cs
+++ HealthApp.Domain/Interfaces/IAppointmentRepository.cs
@@ -4,11 +4,8 @@ namespace HealthApp.Domain.Interfaces;
 
 public interface IAppointmentRepository
 {
-    IQueryable<Appointment> GetAll();
-    Task<Appointment> CreateAppointmentAsync(AppointmentCreateDto dto);
     Task<Appointment> GetByIdAsync(int id);
     Task<IEnumerable<Appointment>> GetAllAsync();
-    IQueryable<Appointment> GetPatientAppointments(string patientId);
     Task<IEnumerable<Appointment>> GetByPatientIdAsync(int patientId);
     Task<IEnumerable<Appointment>> GetByDoctorIdAsync(int doctorId);
     Task<IEnumerable<Appointment>> GetByDateRangeAsync(DateTime start, DateTime end);
@@ -20,6 +17,4 @@ public interface IAppointmentRepository
     Task<IEnumerable<Appointment>> GetPendingAppointmentsAsync(int doctorId);
     Task<int> GetAppointmentCountByStatusAsync(string status);
     Task<bool> CancelAppointmentAsync(int appointmentId, string reason);
-    IEnumerable<object> GetDoctorAppointments(int id);
-    Task<IEnumerable<Appointment>> GetRecentAsync(int count);
 }
