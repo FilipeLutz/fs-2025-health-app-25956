b4efdab0723a94a5d73bf0578ba155d8b6776ad5
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 12:55:20 +0100

    Revert "Update Prescription.cs"
    This reverts commit 82ebd725a87351eeebee09263bcda48b9a078f95.

--- HealthApp.Domain/Entities/Prescription.cs
+++ HealthApp.Domain/Entities/Prescription.cs
@@ -1,43 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace HealthApp.Domain.Entities;
+﻿namespace HealthApp.Domain.Entities;
 
 public class Prescription
 {
     public int Id { get; set; }
-
-    [Required]
     public int AppointmentId { get; set; }
-    public Appointment? Appointment { get; set; }
+    public required Appointment Appointment { get; set; }
 
-    [Required]
-    public int DoctorId { get; set; }
-    public Doctor? Doctor { get; set; }
-
-    [Required]
-    public int PatientId { get; set; }
-    public Patient? Patient { get; set; }
-
-    [Required, MaxLength(100)]
-    public string Medication { get; set; } = null!;
-
-    [Required, MaxLength(50)]
-    public string Dosage { get; set; } = null!;
-
-    [Required, MaxLength(50)]
-    public string Frequency { get; set; } = null!;
-
-    [Required]
-    public int DurationDays { get; set; }
-
+    public string? Medication { get; set; }
+    public string? Dosage { get; set; }
     public string? Instructions { get; set; }
-
-    public bool AllowRefills { get; set; }
-    public int RefillsAllowed { get; set; }
-
-    [Required]
     public DateTime PrescribedDate { get; set; } = DateTime.UtcNow;
-
     public DateTime? ExpiryDate { get; set; }
     public string? Notes { get; set; }
 }
