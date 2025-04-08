5522b9c930d4f629c47b67a62528c1b7bea00c64
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-08 13:07:58 +0100

    Reapply "Update Patient.cs"
    This reverts commit a6143787a3f74fdd4d610f19c677143a37fd2f7d.

--- HealthApp.Domain/Entities/Patient.cs
+++ HealthApp.Domain/Entities/Patient.cs
@@ -1,39 +1,37 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic; 
-using HealthApp.Domain.Entities;
-using HealthApp.Razor.Data;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HealthApp.Domain.Entities;
 
 public class Patient
 {
-    [Key]
     public int Id { get; set; }
 
     [Required]
-    public required string UserId { get; set; }
-
-    [Required, MaxLength(100)]
-    public required string Name { get; set; }
+    [MaxLength(100)]
+    public string FirstName { get; set; }
 
     [Required]
-    public DateTime DateOfBirth { get; set; }
-
-    [MaxLength(20)]
-    public string? PhoneNumber { get; set; }
-
     [MaxLength(100)]
-    public required string Email { get; set; }
+    public string LastName { get; set; }
+
+    public string FullName => $"{FirstName} {LastName}";
 
     [MaxLength(500)]
-    public string? Address { get; set; }
+    public string Address { get; set; }
 
-    public ICollection<DoctorPatient>? DoctorPatients { get; set; }
-    public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
-    public ICollection<Prescription> MedicalRecords { get; set; } = new List<Prescription>();
+    public DateTime DateOfBirth { get; set; }
+    public string BloodType { get; set; }
+    public string Allergies { get; set; }
+    public string MedicalHistory { get; set; }
+    public string InsuranceInfo { get; set; }
 
-    public string? BloodType { get; set; }
-    public double Height { get; set; }
-    public double Weight { get; set; }
-    public string? Allergies { get; set; }
+    [ForeignKey("UserId")]
+    public string UserId { get; set; }
+    public ApplicationUser User { get; set; }
+
+    public ICollection<Appointment> Appointments { get; set; }
+    public ICollection<Prescription> Prescriptions { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
 }
