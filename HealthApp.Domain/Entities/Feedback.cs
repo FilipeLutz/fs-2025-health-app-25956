9b53813dc850c83971d249b59f92fc0d5b21a244
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-18 16:31:25 +0100

    Create Feedback.cs

---
+++ HealthApp.Domain/Entities/Feedback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthApp.Domain.Entities
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int AppointmentId { get; set; }
+
+        public Appointment Appointment { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [MaxLength(1000)]
+        public string Comment { get; set; }
+
+        public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
+    }
+}
