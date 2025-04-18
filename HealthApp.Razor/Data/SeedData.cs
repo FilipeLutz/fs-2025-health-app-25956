8ae8b0741f1504d312fedb2635a076eebaa2069a
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-04-18 16:32:05 +0100

    Update SeedData.cs

--- HealthApp.Razor/Data/SeedData.cs
+++ HealthApp.Razor/Data/SeedData.cs
@@ -30,6 +30,8 @@ public static class SeedData
         {
             adminUser = new ApplicationUser
             {
+                FirstName = "System",
+                LastName = "Admin",
                 UserName = adminEmail,
                 Email = adminEmail,
                 EmailConfirmed = true
@@ -38,62 +40,72 @@ public static class SeedData
             await userManager.AddToRoleAsync(adminUser, "Admin");
         }
 
-        // Seed sample doctors
-        var doctorFaker = new Faker<Doctor>()
-            .RuleFor(d => d.UserId, f => f.Random.Guid().ToString())
-            .RuleFor(d => d.Id, f => f.IndexFaker + 1)
-            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
-            .RuleFor(u => u.LastName, f => f.Name.LastName())
-            .RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName));
-
+        // Seed sample doctors with associated ApplicationUser
         var specialties = new[] { "Cardiology", "Neurology", "Pediatrics", "Orthopedics" };
+        var doctorFaker = new Faker<Doctor>()
+            .RuleFor(d => d.FirstName, f => f.Name.FirstName())
+            .RuleFor(d => d.LastName, f => f.Name.LastName())
+            .RuleFor(d => d.PhoneNumber, f => f.Phone.PhoneNumber())
+            .RuleFor(d => d.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+            .RuleFor(d => d.Specialization, f => f.PickRandom(specialties))
+            .RuleFor(d => d.LicenseNumber, f => $"MD{f.Random.Number(10000, 99999)}");
 
         for (int i = 0; i < 10; i++)
         {
-            var doctorUser = doctorFaker.Generate();
+            var doctor = doctorFaker.Generate();
 
-            var doctor = new Doctor
+            var doctorUser = new ApplicationUser
             {
-                Id = doctorUser.Id,
-                UserId = doctorUser.UserId,
-                FirstName = doctorUser.FirstName,
-                LastName = doctorUser.LastName,
-                Email = doctorUser.Email,
-                PhoneNumber = doctorUser.PhoneNumber,
-                Specialization = specialties[Random.Shared.Next(specialties.Length)],
-                LicenseNumber = $"MD{Random.Shared.Next(10000, 99999)}"
+                FirstName = doctor.FirstName,
+                LastName = doctor.LastName,
+                Email = doctor.Email,
+                UserName = doctor.Email,
+                EmailConfirmed = true
             };
-            context.Doctors.Add(doctor);
+
+            var result = await userManager.CreateAsync(doctorUser, "Doctor@1234");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(doctorUser, "Doctor");
+
+                doctor.UserId = doctorUser.Id;
+                context.Doctors.Add(doctor);
+            }
         }
 
-        // Seed 1000 patients
+        // Seed 1000 patients with associated ApplicationUser
         var patientFaker = new Faker<Patient>()
-            .RuleFor(p => p.Id, f => f.IndexFaker + 1)
-            .RuleFor(p => p.UserId, f => f.Random.Guid().ToString())
             .RuleFor(p => p.FirstName, f => f.Name.FirstName())
             .RuleFor(p => p.LastName, f => f.Name.LastName())
             .RuleFor(p => p.Email, (f, p) => f.Internet.Email(p.FirstName, p.LastName))
             .RuleFor(p => p.PhoneNumber, f => f.Phone.PhoneNumber())
             .RuleFor(p => p.Address, f => f.Address.FullAddress())
-            .RuleFor(p => p.DateOfBirth, f => f.Date.Past(30, DateTime.Now))
-            .RuleFor(p => p.BloodType, f => f.PickRandom(new[] { "A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-" }));
+            .RuleFor(p => p.DateOfBirth, f => f.Date.Past(30, DateTime.Now.AddYears(-18)))
+            .RuleFor(p => p.BloodType, f => f.PickRandom(new[] { "A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-" }))
+            .RuleFor(p => p.Allergies, f => f.Lorem.Word())
+            .RuleFor(p => p.MedicalHistory, f => f.Lorem.Paragraph())
+            .RuleFor(p => p.InsuranceInfo, f => f.Company.CompanyName());
 
         var patients = patientFaker.Generate(1000);
-        foreach (var patientUser in patients)
+        foreach (var patient in patients)
         {
-            var patient = new Patient
+            var patientUser = new ApplicationUser
             {
-                FirstName = patientUser.FirstName,
-                LastName = patientUser.LastName,
-                Email = patientUser.Email,
-                PhoneNumber = patientUser.PhoneNumber,
-                Address = patientUser.Address,
-                DateOfBirth = patientUser.DateOfBirth,
-                BloodType = patientUser.BloodType,
-                MedicalHistory = "No known allergies",
-                InsuranceInfo = "Basic Coverage"
+                FirstName = patient.FirstName,
+                LastName = patient.LastName,
+                Email = patient.Email,
+                UserName = patient.Email,
+                EmailConfirmed = true
             };
-            context.Patients.Add(patient);
+
+            var result = await userManager.CreateAsync(patientUser, "Patient@1234");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(patientUser, "Patient");
+
+                patient.UserId = patientUser.Id;
+                context.Patients.Add(patient);
+            }
         }
 
         await context.SaveChangesAsync();
